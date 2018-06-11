5db6b2ff8222e378fc34caf0b0a4e7560f189090
Isaac Lañdao <isaac.lanado@cnh.gob.mx>
2018-06-11 18:30:22 -0500

    test para sacar totales

--- routes/coins.js
+++ routes/coins.js
@@ -10,6 +10,30 @@ router.get('/list', function (req, res, next) {
   });
 });
 
+router.get('/totales', function (req, res, next) {
+  var db = req.db;
+  var collection = db.get('coins');
+  collection.aggregate([{
+    $group: {
+      _id: {
+        ticker: "$ticker",
+        price: "$price"
+      },
+      "balanceTotal" : {
+        $sum: "$balance"
+      }
+    }
+  }, {
+    $project: {
+      ticker: "$_id.ticker",
+      price: "$_id.price",
+      "balanceTotal": "$balanceTotal"
+    }
+  }, {
+    $skip: 0
+  }])
+})
+
 router.post('/add', function (req, res) {
   var db = req.db;
   var collection = db.get('coins');
