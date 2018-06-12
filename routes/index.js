b0dca433412cdabb1cec353dbcb979662d536d16
Isaac Lañado <ilanado@gmail.com>
2018-06-12 10:04:40 -0500

    currency format

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var db = req.db;
-  var collection = db.get('coins');
-  var query = {
+    var db = req.db;
+    var collection = db.get('coins');
+    var query = {
         "$or": [
             {
                 "name": "MXN"
@@ -15,9 +15,9 @@ router.get('/', function (req, res, next) {
             }
         ]
     };
-  collection.find(query, { sort: { ticker: 1 } }, function (e, docs) {
-    res.render('index', { title: 'Calculo Minar Granja', mxn: docs[0].price, usd: docs[1].price });
-  });
+    collection.find(query, { sort: { ticker: 1 } }, function (e, docs) {
+        res.render('index', { title: 'Calculo Minar Granja', mxn: new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(docs[0].price), usd: new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(docs[1].price) });
+    });
 });
 
 module.exports = router;
